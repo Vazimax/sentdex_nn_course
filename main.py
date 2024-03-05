db5725ee7ca5585e568dff5f47713fe35649174d
vazimax <aboubakr.elhabti@gmail.com>
2024-03-05 20:46:57 +0800

    made it dynamic

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import numpy as np
 
-inputs = [[1,2,3,4],
-          [7,4,-3,0.65],
-          [8.7,0.26,3,-4],
+np.random.seed(0)
+
+
+X = [   [1,2,3,4],
+        [7,4,-3,0.65],
+        [8.7,0.26,3,-4],
 ]
 
-weights = [
-    [5,3.1,6.5,12]
-    ,[0.1,7,0.5,2.4]
-    ,[2,9,3,0.2]
-]
+class LayerDense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs,n_neurons)
+        self.biases = np.zeros((1,n_neurons))
 
-biases = [3.1, 0.8, 2]
+    def forward(self,inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-weights2 = [
-    [1.2,1.1,9.5]
-    ,[0.1,7,2.4]
-    ,[3,7,1.2]
-]
+layer1 = LayerDense(4,2)
+layer2 = LayerDense(2,1)
 
-biases2 = [3.1, 0.8, 2]
-
-output = np.dot(inputs, np.array(weights).T) + biases
-output2 = np.dot(output, np.array(weights2).T) + biases2
-
-print(f'the output is {output}')
+layer1.forward(X)
+layer2.forward(layer1.output)
