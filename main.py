5d7db77ae094ca8f295379cd60d8388482cf2397
vazimax <aboubakr.elhabti@gmail.com>
2024-03-14 23:59:18 +0800

    implemented the entropy loss

--- main.py
+++ main.py
@@ -25,6 +25,28 @@ class SoftmaxActivation:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+class EntropyLoss(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = LayerDense(2,3)
@@ -42,4 +64,7 @@ act2.forward(dense2.output)
 print(act2.output[:3])
 
 
+loss_function = EntropyLoss()
+loss = loss_function.calculate(act2.output, y)
 
+print("Loss:", loss)
